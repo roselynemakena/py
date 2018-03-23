146bc259a902124f7c03ce8178b0173d1d3092bf
makena <roselynemakena@gmail.com>
2018-03-23 10:45:49 +0300

    #Edit pg commands

--- postgres.py
+++ postgres.py
@@ -1,8 +1,16 @@
 # This is a quick cheatsheet of postgres commands that I have found useful over time.
 
+# Install postgres to your ubuntu environment
+
+sudo apt-get update
+sudo apt-get install postgresql
+
+# Check postgres version
+
+psql --version
 
 # Log in to postgres console
-sudo -u postgres psql
+sudo -u postgres psql	
 
 # Create a pg database
 sudo -u postgres createdb <dbname>
